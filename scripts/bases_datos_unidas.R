ccd86e39dbdd349a0bee7ff3a76bc63c9719903d
micaelacz <micaela.cusipuma@pucp.edu.pe>
2026-06-18 12:25:27 -0500

    Generamos nuestro segundo Script para las bases de datos

---
+++ scripts/bases_datos_unidas.R
@@ -0,0 +1,15 @@
+# Cargamos las librerías------------------------------------------------------
+library(readr)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(stringr)
+library(rio)
+library(tidyverse)
+library(janitor)
+
+
+# Usanos Renv-------------------------------------------------------------
+renv::snapshot(force = TRUE)
+
+
